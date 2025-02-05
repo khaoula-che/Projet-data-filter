9ffa4134cec46faed2b66ee1a8ea4522649629c6
Khaoula Chetioui <khaoulachetioui4@gmail.com>
2025-02-05 22:49:06 +0100

    update chargement / sauvegarde

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import csv
-from os import write
 import json
-import yaml
 import xml.etree.ElementTree as ET
+import yaml
+from os import write
 
 # charger des donnees depuis un csv et retoune une liste de dict
 def load_csv(file):
     data = []
-    with open(file, 'r') as f :
+    with open(file, 'r', encoding='utf-8') as f :
         reader = csv.DictReader(f)
         for row in reader : 
             data.append(row)
@@ -18,25 +18,78 @@ def save_csv(file,data):
         print("Aucune donnees à sauvegarder")
         return
     keys = data[0].keys()
-    with open(file, 'w') as f:
+    with open(file, 'w', encoding='utf-8') as f:
         writer = csv.DictWriter(f, file=keys)
         writer.writeheader()
         writer.writerows(data)
 
 def load_json(file):
-    with open(file,"r") as f :
+    with open(file,'r', encoding='utf-8') as f :
         return json.load(f)
     
 def save_json(file, data) :
-    with open(file,"w") as f :
-         json.dump(data,file, indent=4)
+    with open(file,'w',encoding='utf-8') as f :
+        json.dump(data,f, indent=4)
+
+def load_xml(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    data = []
+    for i in root : 
+        item = {child.tag: child.text for child in i}
+        data.append(item)
+    return data
+        
+def save_xml(file,data) :
+    root = ET.Element("root")
+    for i in data :
+        item = ET.SubElement(root,"item")
+        for key,value in item.items():
+            child = ET.SubElement(item,key)
+            child.text = str(value)
+    tree = ET.ElementTree(root)
+    tree.write(file)
+def load_yaml(file) :
+    with open(file, 'r',encoding='utf-8') as f :
+        return yaml.safe_load(f)
+
+def save_yaml(file, data) :
+    with open(file,'w', encoding='utf-8') as f :
+        yaml.dump(data, f,default_flow_style=False)
+def mean(values) :
+    return sum(values) / len(values)
+
+def stats(data) :
+    stats ={}
+    for key in data[0].keys():
+        values = [item[key] for item in data if key in item]
+        values_num = []
+        for v in values : 
+           if isinstance(v, (int, float)):
+                values_num.append(v)
+           elif isinstance(v, str) and v.replace('.', '', 1).isdigit():
+                values_num.append(float(v))
+        if values_num :
+            stats[key] = { 
+                'min': min(values_num), 
+                'max': max(values_num),
+                'mean': mean(values_num)
+            }
+        
+    return stats
 
 file_csv = "data.csv"
 file_json = "data.json"
+file_xml = "data.xml"
+file_yaml = "data.yaml"
 
 data_csv = load_csv(file_csv)
 data_json = load_json(file_json)
-
-print("données CSV chargées ", data_csv)
-print("données JSON chargées ", data_json)
-
+data_xml = load_xml(file_xml)
+data_yaml = load_yaml(file_yaml)
+stats = stats(data_json)
+print("données CSV chargées :", data_csv)
+print("données JSON chargées :", data_json)
+print("données XML chargées :", data_xml)
+print("données YAML chargées :", data_yaml)
+print("Statistiques des données :", stats)
